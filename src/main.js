67cc54ef4448c508c59db2df8593c3805154783f
Callan <cerveauxallanjean@gmail.com>
2018-09-22 17:02:41 +0200

    Reorganization of the structure folder and deletation usless files

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import '@babel/polyfill'
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
+import '@babel/polyfill'
+import './plugins/vuetify'
 
 Vue.config.productionTip = false
 
